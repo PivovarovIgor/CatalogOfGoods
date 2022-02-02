6fe6c6a12369a48ce598e76cf7bc2ea17031e0b9
PivIgor <brauer.igor@gmail.com>
2022-02-03 00:13:08 +0300

    Testing Rx and Flow.

--- app/src/test/java/ru/brauer/catalogofgoods/ui/catalogofgoods/CatalogOfGoodsViewModelTest.kt
+++ app/src/test/java/ru/brauer/catalogofgoods/ui/catalogofgoods/CatalogOfGoodsViewModelTest.kt
@@ -1,25 +1,50 @@
 package ru.brauer.catalogofgoods.ui.catalogofgoods
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.paging.PagingData
-import androidx.paging.cachedIn
-import com.nhaarman.mockito_kotlin.*
-import io.mockk.every
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations.openMocks
+import org.robolectric.annotation.Config
+import ru.brauer.catalogofgoods.TestCoroutineRule
 import ru.brauer.catalogofgoods.data.entities.Goods
 import ru.brauer.catalogofgoods.domain.IRepository
 import ru.brauer.catalogofgoods.stubs.SchedulerProviderStub
+import java.lang.Thread.sleep
 
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+@ExperimentalCoroutinesApi
 class CatalogOfGoodsViewModelTest {
 
     private lateinit var viewModel: CatalogOfGoodsViewModel
+
+    private val lifecycleOwnerFake = LifecycleOwnerFake()
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
 
     @Mock
     private lateinit var repository: IRepository
@@ -32,35 +57,61 @@ class CatalogOfGoodsViewModelTest {
     @Before
     fun setup() {
         mocks = openMocks(this)
-
-        val pagingFlow = mock<Flow<PagingData<Goods>>>()
-
-
-        every { any<Flow<PagingData<Goods>>>().cachedIn(any()) } returns pagingFlow
-
-        `when`(pagingFlow.cachedIn(any())).thenReturn(mock<Flow<PagingData<Goods>>>())
-        `when`(repository.getPagingFlowFromLocalSource(any())).thenReturn(pagingFlow)
-        viewModel = CatalogOfGoodsViewModel(
-            repository = repository,
-            compositeDisposable = compositeDisposable,
-            schedulerProvider = SchedulerProviderStub()
-        )
+        lifecycleOwnerFake.resume()
+        testCoroutineRule.runBlockingTest {
+            val pagingFlow: Flow<PagingData<Goods>> = flow {
+                emit(PagingData.empty())
+            }
+            `when`(repository.getPagingFlowFromLocalSource(any())).thenReturn(pagingFlow)
+            viewModel = CatalogOfGoodsViewModel(
+                repository = repository,
+                compositeDisposable = compositeDisposable,
+                schedulerProvider = SchedulerProviderStub()
+            )
+        }
     }
 
     @Test
     fun loading_CommerceML_test() {
         `when`(repository.getGoods(any())).thenReturn(
             Single.just(
-                listOf(Goods.empty())
+                listOf()
             )
         )
 
-        viewModel.observe(mock(), mock(), mock())
+        viewModel.observe(lifecycleOwnerFake, { }, { })
         verify(repository, times(1)).getGoods(any())
     }
 
     @After
     fun close() {
+        lifecycleOwnerFake.destroy()
         mocks.close()
+    }
+}
+
+class LifecycleOwnerFake : LifecycleOwner {
+
+    private var currentStateFake = Lifecycle.State.RESUMED
+
+    private val lifecycleFake = LifecycleFake()
+
+    private inner class LifecycleFake : Lifecycle() {
+
+        override fun addObserver(observer: LifecycleObserver) {}
+
+        override fun removeObserver(observer: LifecycleObserver) {}
+
+        override fun getCurrentState(): State = currentStateFake
+    }
+
+    override fun getLifecycle(): Lifecycle = lifecycleFake
+
+    fun destroy() {
+        currentStateFake = Lifecycle.State.DESTROYED
+    }
+
+    fun resume() {
+        currentStateFake = Lifecycle.State.RESUMED
     }
 }
