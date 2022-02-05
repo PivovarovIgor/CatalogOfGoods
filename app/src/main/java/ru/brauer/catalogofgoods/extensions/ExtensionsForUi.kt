48d2394edc2693cb7f6296958216ef8c8d928144
PivIgor <brauer.igor@gmail.com>
2022-02-05 20:40:13 +0300

    Optimization transition for appearance photos.

--- app/src/main/java/ru/brauer/catalogofgoods/extensions/ExtensionsForUi.kt
+++ app/src/main/java/ru/brauer/catalogofgoods/extensions/ExtensionsForUi.kt
@@ -8,11 +8,13 @@ import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import ru.brauer.catalogofgoods.R
 import ru.brauer.catalogofgoods.data.glidemodel.FtpModel
 
+private const val TRANSITION_IMAGE_APPEARANCE_DURATION = 180
+private const val ROUNDING_RADIUS_FOR_PHOTO = 25
+
 private val DRAWABLE_CROSS_FADE_FACTORY =
-    DrawableCrossFadeFactory.Builder()
+    DrawableCrossFadeFactory.Builder(TRANSITION_IMAGE_APPEARANCE_DURATION)
         .setCrossFadeEnabled(true)
         .build()
-private const val ROUNDING_RADIUS_FOR_PHOTO = 25
 
 fun ImageView.loadFirstImage(listOfPhotosUri: List<String>) =
     loadFirstImage(listOfPhotosUri.firstOrNull())
