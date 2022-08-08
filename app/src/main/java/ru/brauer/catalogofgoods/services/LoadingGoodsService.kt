bf3671ae725f84ecc264fe9a4551e801c5d77659
PivovarovIV <brauer.igor@gmail.com>
2022-08-08 22:33:10 +0300

    optimizations

--- app/src/main/java/ru/brauer/catalogofgoods/services/LoadingGoodsService.kt
+++ app/src/main/java/ru/brauer/catalogofgoods/services/LoadingGoodsService.kt
@@ -1,9 +1,7 @@
 package ru.brauer.catalogofgoods.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.pm.ServiceInfo
 import android.graphics.BitmapFactory
@@ -12,6 +10,7 @@ import android.os.IBinder
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
@@ -20,16 +19,21 @@ import ru.brauer.catalogofgoods.R
 import ru.brauer.catalogofgoods.domain.BackgroundLoadingState
 import ru.brauer.catalogofgoods.domain.IRepository
 import ru.brauer.catalogofgoods.rx.ISchedulerProvider
+import ru.brauer.catalogofgoods.ui.MainActivity
 import javax.inject.Inject
 
 class LoadingGoodsService : Service() {
 
-    @Inject lateinit var repository: IRepository
-    @Inject lateinit var compositeDisposable: CompositeDisposable
-    @Inject lateinit var schedulerProvider: ISchedulerProvider
+    @Inject
+    lateinit var repository: IRepository
+    @Inject
+    lateinit var compositeDisposable: CompositeDisposable
+    @Inject
+    lateinit var schedulerProvider: ISchedulerProvider
 
     private var disposable: Disposable? = null
     private var notificationService: NotificationManagerCompat? = null
+    private var notificationBuilder: NotificationCompat.Builder? = null
 
     private val processingLoadingObserver = object : Observer<BackgroundLoadingState.LoadingState> {
 
@@ -43,10 +47,9 @@ class LoadingGoodsService : Service() {
         }
 
         override fun onNext(processing: BackgroundLoadingState.LoadingState) {
-           // backgroundProcessing.postValue(processing)
+            // backgroundProcessing.postValue(processing)
             val not = createNotification(processing.count.toString())
             notificationService?.notify(NOTIFY_ID, not)
-            println("Loading ${processing.count}")
         }
 
         override fun onError(exeption: Throwable) {
@@ -66,10 +69,13 @@ class LoadingGoodsService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //val PendingIntent: PendingIntent = Inte
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(NOTIFY_ID, createNotification("started"), ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+            startForeground(
+                NOTIFY_ID,
+                createNotification("started"),
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            )
         }
         beginLoadingData()
         return START_STICKY
@@ -81,7 +87,11 @@ class LoadingGoodsService : Service() {
             val notCan =
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     val channelName = getString(R.string.channel_name_of_loading_service)
-                    NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH)
+                    NotificationChannel(
+                        CHANNEL_ID,
+                        channelName,
+                        NotificationManager.IMPORTANCE_HIGH
+                    )
                         .apply {
                             enableLights(true)
                             enableVibration(false)
@@ -97,19 +107,37 @@ class LoadingGoodsService : Service() {
             notificationService?.createNotificationChannel(notCan)
         }
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle(text)
-                .setSmallIcon(android.R.drawable.stat_sys_download)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_cloud_download_24))
-                .setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
-                .setOngoing(true)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setTicker(text)
-                .setOnlyAlertOnce(true)
-                .setProgress(0,0,true)
-                .setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
-                .setCategory(Notification.CATEGORY_PROGRESS)
-                .build()
+
+            if (notificationBuilder == null) {
+                val pendingIntent = Intent(this, MainActivity::class.java)
+                    .let { intent ->
+                        TaskStackBuilder.create(this)
+                            .addNextIntent(intent)
+                    }.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+                notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(android.R.drawable.stat_sys_download)
+                    .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                            resources,
+                            R.drawable.ic_baseline_cloud_download_24
+                        )
+                    )
+                    .setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
+                    .setOngoing(true)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(false)
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    .setOnlyAlertOnce(true)
+                    .setProgress(0, 0, true)
+                    .setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
+                    .setCategory(Notification.CATEGORY_PROGRESS)
+            }
+            notificationBuilder?.let {
+                it.setContentTitle(text)
+                it.setTicker(text)
+                it.build()
+            } ?: throw IllegalStateException()
+
         } else {
             TODO("VERSION.SDK_INT < S")
         }
@@ -131,10 +159,18 @@ class LoadingGoodsService : Service() {
             .getGoods(processingLoadingObserver)
             .observeOn(schedulerProvider.ui())
             .doOnDispose {
-                Toast.makeText(applicationContext, "Loading is stop", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.updating_data_is_stop),
+                    Toast.LENGTH_LONG
+                ).show()
             }
             .subscribe({
-                Toast.makeText(applicationContext, "Loading is success", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.updating_data_is_start),
+                    Toast.LENGTH_LONG
+                ).show()
             }, {
                 Toast.makeText(applicationContext, it.message, Toast.LENGTH_LONG).show()
             }).also { compositeDisposable.add(it) }
